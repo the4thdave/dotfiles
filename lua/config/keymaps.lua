e71c0aac85e943bdbea1047cdf0820f97829e99a
David Munoz <7dmunoz@gmail.com>
2024-03-25 10:24:49 -0700

    Add & remove keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,11 @@ vim.keymap.set('n', '<leader>do', ':DiffviewOpen<CR>', {})
 vim.keymap.set('n', '<leader>df', ':DiffviewFileHistory<CR>', {})
 
 vim.keymap.set('n', '<leader>cc', ':tabclose<CR>', {})
+
+-- Buffers
+vim.keymap.set('n', '[b', ':bprev<CR>', {})
+vim.keymap.set('n', ']b', ':bnext<CR>', {})
+
+-- Quickfix
+vim.keymap.set('n', '[q', ':cprev<CR>', {})
+vim.keymap.set('n', ']q', ':cnext<CR>', {})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,7 +5,6 @@ vim.cmd("set incsearch")
 vim.cmd("set hlsearch")
 vim.cmd("set number")
 vim.cmd("set splitright")
-vim.cmd("nnoremap <S-Tab> <C-w>w")
 vim.cmd("set colorcolumn=120")
 
 -- vim-commentary
