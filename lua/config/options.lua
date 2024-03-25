3380632a20c4d80215ab81eed195bcb4e25e1475
David Munoz <david_munoz@gap.com>
2024-03-25 10:25:58 -0700

    Use spaces instead of tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,15 @@
+-- Defines the amount of whitespace used for one level of indentation
 vim.cmd("set shiftwidth=2")
+
+-- Determines the width of a tab character in columns
 vim.cmd("set tabstop=2")
+
+-- Use spaces instead of tabs
+vim.cmd("set expandtab")
+
+-- Amount of whitespace to be inserted and removed
 vim.cmd("set softtabstop=2")
+
 vim.cmd("set incsearch")
 vim.cmd("set hlsearch")
 vim.cmd("set number")
